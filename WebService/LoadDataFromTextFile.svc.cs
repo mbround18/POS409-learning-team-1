c4d6a4f26dac8f1e1ac4be6a2714d6c3fa02e29c
Khai Hoang <khai.hoang@outlook.com>
2019-09-16 23:58:37 -0400

    add webservice

---
+++ WebService/LoadDataFromTextFile.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WebService.Models;
+using System.IO;
+using System.Web.Services;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LoadDataFromTextFile" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select LoadDataFromTextFile.svc or LoadDataFromTextFile.svc.cs at the Solution Explorer and start debugging.
+    public class LoadDataFromTextFile : ILoadDataFromTextFile
+    {
+        //public void DoWork()
+        //{
+        //}
+        [WebMethod]
+        public List<Address> LoadData()
+        {
+            string dataFilePath = @"MOCK_DATA.csv";
+            var query = from line in File.ReadAllLines(dataFilePath).Skip(1)
+                        where line.Length > 1
+                        select Address.ParseFromTextFile(line);
+            return query.ToList();
+        }
+
+  
+    }
+}
---
+++ WebService/Models/Address.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebService.Models
+{
+
+    public class Address
+    {
+        public Guid id;
+        public string Address1;
+        public string Address2;
+        public string City;
+        public string State;
+        public string Country;
+        public string ZipCode;
+
+        public int Longtitude { get; set; }
+        public int Latitude { get; set; }
+
+        //public Address(string address1, string address2 = "", string city = "", string state = "", string country = "", string zipCode = "")
+        //{
+        //    id = Guid.NewGuid();
+        //    Address1 = address1;
+        //    Address2 = address2;
+        //    City = city;
+        //    State = state;
+        //    Country = country;
+        //    ZipCode = zipCode;
+        //}
+
+        private List<string> GetPropList()
+        {
+            return new List<string>() { id + "", Address1, Address2, City, State, Country, ZipCode };
+        }
+
+        public new string ToString()
+        {
+            return string.Join(", ", ToArray());
+        }
+        public string[] ToArray()
+        {
+            return GetPropList().ToArray();
+        }
+
+        internal static Address ParseFromTextFile(string line)
+        {
+            var fields = line.Split(',');
+
+            return new Address
+            {
+                id = Guid.NewGuid(),
+                Address1 = fields[1].Trim(),
+                Address2 = fields[2].Trim(),
+                City = fields[3].Trim(),
+                State = fields[4].Trim(),
+                Country = fields[5],
+                ZipCode = fields[6]
+            };
+
+        }
+    }
+}
